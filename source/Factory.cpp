15400dfbfb40fa402ea21066fb1ccd8dce2f3941
timslator <tim.slator@base2s.com>
2022-12-12 12:03:14 -0800

    fixed color swap

--- source/Factory.cpp
+++ source/Factory.cpp
@@ -4,11 +4,11 @@ namespace factory
 {
     Logger *CreateCarLogger()
     {
-        return new ColorLogger(ColorLogger::Color::Red);
+        return new ColorLogger(ColorLogger::Color::Blue);
     }
 
     Logger *CreateFuelGaugeLogger()
     {
-        return new ColorLogger(ColorLogger::Color::Blue);
+        return new ColorLogger(ColorLogger::Color::Red);
     }
 }
